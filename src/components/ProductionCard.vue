660bdfbcc71b24b8f587387cab46ad5ee6bad944
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-08 13:53:21 +0000

    Added Flag

--- src/components/ProductionCard.vue
+++ src/components/ProductionCard.vue
@@ -3,6 +3,17 @@ export default {
     name: 'ProductionCard',
     props: {
         item: Object
+    },
+    computed: {
+        hasFlag() {
+            const flags = ['it', 'en', 'pl'];
+            return flags.includes(this.item.original_language);
+        },
+        flagSrc() {
+            const url = new URL(`../assets/img/${this.item.original_language}.png`, import.meta.url);
+
+            return url.href;
+        }
     }
 };
 </script>
@@ -11,9 +22,16 @@ export default {
     <ul>
         <li>{{ item.title || item.name }} </li>
         <li>{{ item.original_title || item.original_name }}</li>
-        <li>{{ item.original_language }}</li>
+        <li>
+            <img v-if="hasFlag" :src="flagSrc" :alt="item.original_language">
+            <span v-else> {{ item.original_language }} </span>
+        </li>
         <li>{{ Math.floor(item.vote_average) }} </li>
     </ul>
 </template>
 
-<style ></style>
+<style scoped >
+img {
+    max-width: 100px;
+}
+</style>
